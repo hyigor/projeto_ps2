b6680bff3c65eeb576efe540758103feb8e91b07
caiaao <31911374@mackenzista.com.br>
2020-04-23 15:21:10 -0300

    setimo  commit

--- ProjetoPs2/src/main/java/br/mack/ps2/persistencia/MySQLConnection.java
+++ ProjetoPs2/src/main/java/br/mack/ps2/persistencia/MySQLConnection.java
@@ -1,5 +1,22 @@
 package br.mack.ps2.persistencia;
 
-public class MySQLConnection {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLDataException;
+import java.sql.SQLException;
 
+public class MySQLConnection {
+    String url = "jdbc:mysql://localhost:3306/projeto_ps2?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+    String usuario = "root";
+    String psw = "root";
+
+    public Connection getConnection() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            return DriverManager.getConnection(url, usuario, psw);
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
 }
